5d4b88b2e5b1d506128b29346881b03ba9c3238e
SantiagoRodriguezP <santiago.rodriguez@sinco.com.co>
2023-08-10 16:40:11 -0500

    Hook personalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,12 @@
-import {useQuery} from '@tanstack/react-query'
+
 
 import './App.css'
-const getRandoNumberFromApi =async () :Promise<number> => {
-  const res = await fetch('https://www.random.org/integers/?num=1&min=1&max=500&col=1&base=10&format=plain&rnd=new');
-  const numberString = await res.text();
-  
-  // throw new Error("Auxilioo!");
-  return +numberString;
-}
+import { useRandom } from './hooks/useRandom'
+
 
 function App() {
- const query = useQuery(
-  ['randomNumber'],
-  getRandoNumberFromApi,
-  
- );
-  
+  const query = useRandom();
+
   return (
   <div>
     {
---
+++ src/hooks/useRandom.tsx
@@ -0,0 +1,19 @@
+import {useQuery} from '@tanstack/react-query'
+
+const getRandoNumberFromApi =async () :Promise<number> => {
+    const res = await fetch('https://www.random.org/integers/?num=1&min=1&max=500&col=1&base=10&format=plain&rnd=new');
+    const numberString = await res.text();
+    
+    // throw new Error("Auxilioo!");
+    return +numberString;
+  }
+export const useRandom = () => {
+    
+    const query = useQuery(
+        ['randomNumber'],
+        getRandoNumberFromApi,
+       );
+    
+       return query;
+        
+}
